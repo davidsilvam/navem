01d7aac36518d89c97efc69374b57e411d5e6454
Unknown <david-silvam@hotmail.com>
2020-12-28 21:36:27 -0300

    gradcam and feature map

---
+++ grad-cam/feature_map.py
@@ -0,0 +1,61 @@
+# visualize feature maps output from each block in the vgg model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import Model
+from matplotlib import pyplot
+from numpy import expand_dims
+import os
+from keras.models import model_from_json
+
+exp_name = "exp_065"
+path_model_json = os.path.join("..\\..\\experiments", exp_name, "model_struct.json")
+path_model = os.path.join("..\\..\\experiments", exp_name, "weights_100.h5") # model_weights_149 _exp_66
+
+# load json and create model
+json_file = open(path_model_json, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights(path_model)
+print("Loaded model from disk")
+
+# load the model
+# model = VGG16()
+model = loaded_model
+# redefine model to output right after the first hidden layer
+ixs = [2, 5, 9, 13, 17]
+outputs = [model.layers[i].output for i in ixs]
+model = Model(inputs=model.inputs, outputs=outputs)
+# load the image with the required shape
+set_n = "vgg\\train\\images"
+img_name = "000097.jpg"
+
+img_path = os.path.join(set_n, img_name)
+img = load_img(img_path, target_size=(224, 224))
+# convert the image to an array
+img = img_to_array(img)
+# expand dimensions so that it represents a single 'sample'
+img = expand_dims(img, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+img = preprocess_input(img)
+# get feature map for first hidden layer
+feature_maps = model.predict(img)
+# plot the output from each block
+square = 8
+for fmap in feature_maps:
+	# plot all 64 maps in an 8x8 squares
+	ix = 1
+	for _ in range(square):
+		for _ in range(square):
+			# specify subplot and turn of axis
+			ax = pyplot.subplot(square, square, ix)
+			ax.set_xticks([])
+			ax.set_yticks([])
+			# plot filter channel in grayscale
+			pyplot.imshow(fmap[0, :, :, ix-1], cmap='gray')
+			ix += 1
+	# show the figure
+	pyplot.show()
---
+++ grad-cam/gradcam.py
@@ -0,0 +1,61 @@
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import keras.backend as K
+import numpy as np
+import cv2
+import sys
+import os
+from keras.models import model_from_json
+
+exp_name = "exp_065"
+path_model_json = os.path.join("..\\..\\experiments", exp_name, "model_struct.json")
+path_model = os.path.join("..\\..\\experiments", exp_name, "weights_100.h5") # model_weights_149 exp_066
+
+# load json and create model
+json_file = open(path_model_json, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights(path_model)
+print("Loaded model from disk")
+
+# model = VGG16(weights="imagenet")
+model = model = loaded_model
+#img_path = sys.argv[1]
+
+set_n = "vgg\\train\\images"
+img_name = "000117.jpg"
+
+img_path = os.path.join(set_n, img_name)
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+class_idx = np.argmax(preds[0])
+class_output = model.output[:, class_idx]
+last_conv_layer = model.get_layer("block5_conv3")
+
+grads = K.gradients(class_output, last_conv_layer.output)[0]
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+
+pooled_grads_value, conv_layer_output_value = iterate([x])
+
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+heatmap = np.maximum(heatmap, 0)
+heatmap /= np.max(heatmap)
+
+img = cv2.imread(img_path)
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+heatmap = np.uint8(255 * heatmap)
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+cv2.imshow("Original", img)
+cv2.imshow("GradCam", superimposed_img)
+cv2.waitKey(0)
